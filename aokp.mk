2dcb0bbd571dfbe168c05c71f784b0ec17e8a463
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2016-05-01 08:49:46 +0200

    AOKP-fy
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/nextbit/ether/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_ether
+PRODUCT_NAME := aokp_ether
 BOARD_VENDOR := nextbit
 TARGET_VENDOR := nextbit
 PRODUCT_DEVICE := ether
